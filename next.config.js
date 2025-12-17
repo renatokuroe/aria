f3b6b92b9260964cff0d5f0d446d853eb7fe4cb8
Renato Kuroe <renatolangley@gmail.com>
2025-12-16 22:57:39 -0300

    Add CSS support to webpack config

--- next.config.js
+++ next.config.js
@@ -1,6 +1,12 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-  // Remove output: 'standalone' para usar dev mode
+  webpack: (config) => {
+    config.module.rules.push({
+      test: /\.css$/,
+      use: ['style-loader', 'css-loader'],
+    })
+    return config
+  },
 }
 
 module.exports = nextConfig
